1c70468ae23da9c746816e5526ba1feb9dd2300d
Mark Renan <markrenan.21@gmail.com>
2023-10-19 22:47:48 +0800

    build: Add apps

--- plmfacultyevaluation/settings.py
+++ plmfacultyevaluation/settings.py
@@ -37,6 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'interface',
+    'user',
+    'document',
+    'evaluation',
+    'report',
+    'ml',
+    'integration',
+    'utility'
 ]
 
 MIDDLEWARE = [
