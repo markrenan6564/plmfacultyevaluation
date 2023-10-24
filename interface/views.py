6c12ac440ce519ed04270cca7aba20fc9199a20f
Denzel Clyde Lim <dcflim2020@plm.edu.ph>
2023-10-24 15:05:57 +0800

    Excluded urls on Header

--- interface/views.py
+++ interface/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 
+excluded_urls = ['home.html', 'signup.html']
+
 # Create your views here.
 def homepage(request):
-	return render(request = request, template_name="interface/home.html")
+	return render(request = request, template_name="interface/home.html", context={"excluded_urls": excluded_urls})
 
 def signup(request):
-	return render(request, template_name="interface/signup.html")
+	return render(request, template_name="interface/signup.html", context={"excluded_urls": excluded_urls})
 
 def dashboard(request):
-	return render(request, template_name="interface/dashboard.html")
+	return render(request, template_name="interface/dashboard.html", context={"excluded_urls": excluded_urls})
+
+def profile(request):
+	return render(request, template_name="interface/profile.html", context={"excluded_urls": excluded_urls})
+
+
